936463bb982459232b47a712bec1b68581efe75d
Morad Bezzazi <mbezzazi@UBUNTU-15.insacvl.intra.insa-cvl.fr>
2024-06-19 09:04:25 +0200

    update4

--- os/challenges/Network/lvl1/server.js
+++ os/challenges/Network/lvl1/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');  // Importation de path pour travailler avec les chemins de fichiers
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.json());
  
@@ -17,8 +17,7 @@ app.get('/net_lvl1', (req, res) => {
     res.sendFile(path.resolve(__dirname, './net_lvl1/network1.html'));
 });
 
-// Démarrage du serveur
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Backend API listening at http://0.0.0.0:${port}`);
+// Démarrer le serveur
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
-
