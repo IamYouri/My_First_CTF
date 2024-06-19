a24981d8891bb10fa6d5e96bd5fdc65ae8c33e94
Morad Bezzazi <mbezzazi@UBUNTU-13.insacvl.intra.insa-cvl.fr>
2024-06-19 21:21:26 +0200

    update14

--- os/challenges/build_docker_image.sh
+++ os/challenges/build_docker_image.sh
@@ -12,8 +12,8 @@ sudo docker build -t challenge_image_8 ./system/lvl3
 sudo docker build -t challenge_image_9 ./system/lvl4
 sudo docker build -t challenge_image_10 ./system/lvl5
 
-# sudo docker build -t challenge_image_11 ./web/lvl1
-# sudo docker build -t challenge_image_12 ./web/lvl2
+sudo docker build -t challenge_image_11 ./web/lvl1
+sudo docker build -t challenge_image_12 ./web/lvl2
 # sudo docker build -t challenge_image_13 ./web/lvl3
 # sudo docker build -t challenge_image_14 ./web/lvl4
 # sudo docker build -t challenge_image_15 ./web/lvl5
