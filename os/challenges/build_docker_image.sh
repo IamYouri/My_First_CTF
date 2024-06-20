e9bf0eff6ed99e7c3d1c4103bd76d950c5256c92
Morad Bezzazi <mbezzazi@UBUNTU-12.insacvl.intra.insa-cvl.fr>
2024-06-20 12:01:06 +0200

    3.3

--- os/challenges/build_docker_image.sh
+++ os/challenges/build_docker_image.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 sudo docker build -t challenge_image_1 ./Network/lvl1
-sudo docker build -t challenge_image_2 ./Network/lvl2
-sudo docker build -t challenge_image_3 ./Network/lvl3
-sudo docker build -t challenge_image_4 ./Network/lvl4
-sudo docker build -t challenge_image_5 ./Network/lvl5
+sudo docker build -t challenge_image_4 ./Network/lvl2
+sudo docker build -t challenge_image_7 ./Network/lvl3
+sudo docker build -t challenge_image_10 ./Network/lvl4
+sudo docker build -t challenge_image_13 ./Network/lvl5
 
-sudo docker build -t challenge_image_6 ./system/lvl1
-sudo docker build -t challenge_image_7 ./system/lvl2
-sudo docker build -t challenge_image_8 ./system/lvl3
-sudo docker build -t challenge_image_9 ./system/lvl4
-sudo docker build -t challenge_image_10 ./system/lvl5
+sudo docker build -t challenge_image_2 ./web/lvl1
+sudo docker build -t challenge_image_5 ./web/lvl2
+sudo docker build -t challenge_image_8 ./web/lvl3
+sudo docker build -t challenge_image_11 ./web/lvl4
+sudo docker build -t challenge_image_14 ./web/lvl5
 
-sudo docker build -t challenge_image_11 ./web/lvl1
-sudo docker build -t challenge_image_12 ./web/lvl2
-sudo docker build -t challenge_image_13 ./web/lvl3
-sudo docker build -t challenge_image_14 ./web/lvl4
-# sudo docker build -t challenge_image_15 ./web/lvl5
+sudo docker build -t challenge_image_3 ./system/lvl1
+sudo docker build -t challenge_image_6 ./system/lvl2
+sudo docker build -t challenge_image_9 ./system/lvl3
+sudo docker build -t challenge_image_12 ./system/lvl4
+sudo docker build -t challenge_image_15 ./system/lvl5
+          
