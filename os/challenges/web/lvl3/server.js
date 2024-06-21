b0e7ca3fc4d6dcab8bf9da4ca767f8e15a0fa8c7
Morad Bezzazi <mbezzazi@UBUNTU-11.insacvl.intra.insa-cvl.fr>
2024-06-21 15:04:02 +0200

    updateY.1

--- os/challenges/web/lvl3/server.js
+++ os/challenges/web/lvl3/server.js
@@ -22,49 +22,13 @@ app.get('/web_lvl3', (req, res) => {
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Page de connexion
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/web_lvl3/login.html');
-});
-
-// Page du challenge 5
-app.get('/challenge5page', (req, res) => {
-    res.sendFile(__dirname + '/web_lvl3/challenge5.html');
-});
-
-// Traitement du formulaire de connexion
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    // Vérification des informations de connexion
-    if (username === 'admin' && password === 'admin') {
-        // Créer un cookie contenant le mot de passe
-        res.cookie('password', 'YouriGoatest', { path: '/', httpOnly: true });
-        res.redirect('/challenge5page');
-    } else {
-        res.redirect('/login');
-    }
-});
-
 // Route pour le challenge 5
 app.get('/challenge5', (req, res) => {
-    if (req.cookies.password === 'YouriGoatest') {
-        // Ajouter un cookie pour le flag du challenge 5
-        res.cookie('flag5', 'CTF{challenge_5_flag}', { path: '/' });
-        res.send('Bienvenue au Challenge 5. Fouillez vos cookies pour trouver le flag.');
-    } else {
-        res.redirect('/login');
-    }
+    // Ajouter un cookie pour le flag du challenge 5
+    res.cookie('flag5', 'CTF{challenge_5_flag}', { path: '/' });
+    res.send('Blank... rienn');
 });
 
-// Page du drapeau (flag)
-app.get('/flag', (req, res) => {
-    if (req.cookies.password === 'YouriGoatest') {
-        res.send('Félicitations ! Voici votre flag : CTF{flag_example}');
-    } else {
-        res.redirect('/login');
-    }
-});
 // ***************
 // Démarrer le serveur
 app.listen(port, () => {
