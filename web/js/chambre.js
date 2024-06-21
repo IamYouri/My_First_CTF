3f73b81a10d5d64dee79d61e23f21e20e77188ff
Morad Bezzazi <mbezzazi@UBUNTU-11.insacvl.intra.insa-cvl.fr>
2024-06-21 15:30:17 +0200

    updateY.1

--- web/js/chambre.js
+++ web/js/chambre.js
@@ -267,7 +267,7 @@ function handleRoomTransition(etageNum, chambreNum, securityString) {
             document.getElementById('next-level-message').style.display = 'block';
             document.getElementById('next-level-message').innerHTML = `
                 <p>Félicitations ! Vous pouvez maintenant accéder à la salle du trésor</p>
-                
+                <button onclick="goToRoom('/felicitation.html')">Aller au trésor</button>
             `;
         }
     }else{
