a46ddf69c160dc939bb6c7d75cc53d95094b7f9a
IamYouri <yourimrk@gmail.com>
2024-06-23 16:35:36 +0200

    changement

--- Breaks/web/cookies2/server.js
+++ Breaks/web/cookies2/server.js
@@ -5,22 +5,26 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 7000;
 
+// Middleware pour analyser les cookies et le corps des requêtes
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// Page de connexion
+
+// Route de connexion
 app.get('/', (req, res) => {
+    // Définition du cookie avec des options supplémentaires
+    res.cookie('flag5', 'CTF{challenge_5_flag}', { 
+        path: '/', // Le cookie est disponible pour toutes les routes
+        httpOnly: true, // Le cookie n'est pas accessible via JavaScript (pour des raisons de sécurité)
+        maxAge: 24 * 60 * 60 * 1000 // Le cookie expire après 1 jour
+    });
+    
+    // Envoi du fichier HTML comme réponse
     res.sendFile(__dirname + '/public/challenge5.html');
 });
 
-// Route pour le challenge 5
-app.get('/challenge5', (req, res) => {
-    // Ajouter un cookie pour le flag du challenge 5
-    res.cookie('flag5', 'CTF{challenge_5_flag}', { path: '/' });
-    res.send('Blank... rienn');
-});
-
+// Démarrage du serveur
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
